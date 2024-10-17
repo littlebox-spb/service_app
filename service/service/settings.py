28d6747be9cdbb0c82a8b166f765d5caf22a3387
Alexander Altukhov <sasha@littlebox.spb.ru>
2024-10-17 21:39:06 +0300

    Celery + redis 	modified:   requirements.txt 	new file:   service/__init__.py 	new file:   service/celery_app.py 	modified:   service/service/settings.py

--- service/service/settings.py
+++ service/service/settings.py
@@ -141,3 +141,6 @@ LOGGING = {
     "handlers": {"console": {"class": "logging.StreamHandler"}},
     "loggers": {"django.db.backends": {"handlers": ["console"], "level": "DEBUG"}},
 }
+
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
