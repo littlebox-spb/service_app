53f2babf0a089aceb449c416823025c99422af11
Alexander Altukhov <sasha@littlebox.spb.ru>
2024-10-17 17:49:41 +0300

    queryset annotate 	modified:   service/services/serializers.py 	modified:   service/services/views.py

--- service/services/views.py
+++ service/services/views.py
@@ -3,17 +3,25 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import Subscription
 from .serializers import SubscriptionSerializer
 from clients.models import Client
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all().prefetch_related(
-        "plan",
-        Prefetch(
-            "client",
-            queryset=Client.objects.all()
-            .select_related("user")
-            .only("company_name", "user__email"),
-        ),
+    queryset = (
+        Subscription.objects.all()
+        .prefetch_related(
+            "plan",
+            Prefetch(
+                "client",
+                queryset=Client.objects.all()
+                .select_related("user")
+                .only("company_name", "user__email"),
+            ),
+        )
+        .annotate(
+            price=F("service__full_price")
+            - F("service__full_price") * F("plan__discount_percent") / 100.00
+        )
     )
+
     serializer_class = SubscriptionSerializer
