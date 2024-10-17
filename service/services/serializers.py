e958edc9f74848435695bdfc9a2c7de8269952de
Alexander Altukhov <sasha@littlebox.spb.ru>
2024-10-17 15:37:57 +0300

    Include serializers 	modified:   service/services/serializers.py 	modified:   service/services/views.py

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,16 +1,19 @@
-from .models import Subscription
 from rest_framework import serializers
+from .models import Plan, Subscription
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = "__all__"
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
+
     client_name = serializers.CharField(source="client.company_name")
     email = serializers.EmailField(source="client.user.email")
 
     class Meta:
         model = Subscription
-        fields = (
-            "id",
-            "plan_id",
-            "client_name",
-            "email",
-        )
+        fields = ("id", "plan_id", "client_name", "email", "plan")
--- service/services/views.py
+++ service/services/views.py
@@ -8,11 +8,12 @@ from django.db.models import Prefetch
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
+        "plan",
         Prefetch(
             "client",
             queryset=Client.objects.all()
             .select_related("user")
             .only("company_name", "user__email"),
-        )
+        ),
     )
     serializer_class = SubscriptionSerializer
